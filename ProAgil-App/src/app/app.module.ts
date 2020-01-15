40c5c4839802ce20d7bb6f9d5fe806a09a681bc0
Leandro Elias Bichara <djleb@NOTEMAC.local>
2020-01-15 01:20:36 -0300

    ProAgil-App: Correçao de pedencias e instalacao ngx-bootstrap

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule  } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule, TooltipModule, ModalModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { EventoService } from './services/evento.service';
 
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from "./nav/nav.component";
-import { EventoService } from './services/evento.service';
+
 import { DateTimeFormatPipePipe } from './helps/DateTimeFormatPipe.pipe';
 
 @NgModule({
@@ -20,9 +24,11 @@ import { DateTimeFormatPipePipe } from './helps/DateTimeFormatPipe.pipe';
    imports: [
       BrowserModule,
       FormsModule,
-      HttpModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot(),
+      TooltipModule.forRoot(),
+      ModalModule.forRoot()
    ],
    providers: [
      // EventoService
